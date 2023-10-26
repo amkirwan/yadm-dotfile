e10f728799c7269d64ec014fa724d5c88c5779a9
akirwan <akirwan@uber.com>
2023-10-26 09:12:05 -0500

    update lazy.nvim plugins

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -31,17 +31,17 @@ vim.keymap.set(
 vim.keymap.set("n", "<leader>bc", ":BufferLinePick<CR>", { desc = "Pick buffer by char" })
 vim.keymap.set("n", "<leader>bC", ":BufferLinePickClose<CR>", { desc = "Close buffer by char " })
 
-vim.keymap.set("n", "<silent>bse", ":BufferLineSortByExtension<CR>", { desc = "Sort buffers by extension" })
-vim.keymap.set("n", "<silent>bsd", ":BufferLineSortByDirectory<CR>", { desc = "Sort buffers by dir" })
+vim.keymap.set("n", "<leader>bse", ":BufferLineSortByExtension<CR>", { desc = "Sort buffers by extension" })
+vim.keymap.set("n", "<leader>bsd", ":BufferLineSortByDirectory<CR>", { desc = "Sort buffers by dir" })
 
-vim.keymap.set("n", "<leader>1", '<cmd>lua require("bufferline").go_to(1, true)<cr>', { desc = "Buffer 1" })
-vim.keymap.set("n", "<leader>2", '<cmd>lua require("bufferline").go_to(2, true)<cr>', { desc = "Buffer 2" })
-vim.keymap.set("n", "<leader>3", '<cmd>lua require("bufferline").go_to(3, true)<cr>', { desc = "Buffer 3" })
-vim.keymap.set("n", "<leader>5", '<cmd>lua require("bufferline").go_to(5, true)<cr>', { desc = "Buffer 4" })
-vim.keymap.set("n", "<leader>6", '<cmd>lua require("bufferline").go_to(6, true)<cr>', { desc = "Buffer 5" })
-vim.keymap.set("n", "<leader>7", '<cmd>lua require("bufferline").go_to(7, true)<cr>', { desc = "Buffer 6" })
-vim.keymap.set("n", "<leader>8", '<cmd>lua require("bufferline").go_to(8, true)<cr>', { desc = "Buffer 7" })
-vim.keymap.set("n", "<leader>9", '<cmd>lua require("bufferline").go_to(9, true)<cr>', { desc = "Buffer 9" })
+-- vim.keymap.set("n", "<leader>1", '<cmd>lua require("bufferline").go_to(1, true)<cr>', { desc = "Buffer 1" })
+-- vim.keymap.set("n", "<leader>2", '<cmd>lua require("bufferline").go_to(2, true)<cr>', { desc = "Buffer 2" })
+-- vim.keymap.set("n", "<leader>3", '<cmd>lua require("bufferline").go_to(3, true)<cr>', { desc = "Buffer 3" })
+-- vim.keymap.set("n", "<leader>5", '<cmd>lua require("bufferline").go_to(5, true)<cr>', { desc = "Buffer 4" })
+-- vim.keymap.set("n", "<leader>6", '<cmd>lua require("bufferline").go_to(6, true)<cr>', { desc = "Buffer 5" })
+-- vim.keymap.set("n", "<leader>7", '<cmd>lua require("bufferline").go_to(7, true)<cr>', { desc = "Buffer 6" })
+-- vim.keymap.set("n", "<leader>8", '<cmd>lua require("bufferline").go_to(8, true)<cr>', { desc = "Buffer 7" })
+-- vim.keymap.set("n", "<leader>9", '<cmd>lua require("bufferline").go_to(9, true)<cr>', { desc = "Buffer 9" })
 
 -- lsp diagnostic to show in popup when text is too long
 vim.keymap.set(
@@ -58,3 +58,10 @@ vim.keymap.set("n", "<leader>sxw", '<cmd>lua require("spectre").open_visual({sel
 vim.keymap.set("n", "<leader>sxp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
   desc = "Search on current file (Spectre)",
 })
+
+vim.keymap.set(
+  "n",
+  "<leader>fs",
+  require("telescope").extensions.live_grep_args.live_grep_args,
+  { desc = "live args", noremap = true }
+)

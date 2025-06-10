1d5db223e54915f89d8dbd8a375318233683dca2
akirwan <akirwan@uber.com>
2025-06-10 00:21:03 -0500

    fzf shell integration

---
+++ .fzf.bash
@@ -0,0 +1,9 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+fi
+
+# Key bindings
+# ------------
+eval "$(fzf --bash)"
